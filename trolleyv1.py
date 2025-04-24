09e3dcd15c92dcccf2cbf8ca637d68eb7508da97
kcidiaz <kiara_ibanez-diaz@brown.edu>
2025-04-24 10:28:42 -0400

    Kiara: Creating Screen
    Testing button and label mechanics with tutorials

--- trolleyv1.py
+++ trolleyv1.py
@@ -1,1 +1,17 @@
-print("start here")
+from tkinter import*
+
+root = Tk()
+root.title("The Trolley Problem")
+root.attributes('-fullscreen', True)
+
+
+my_label = Label(root, text = "Hello World!", font = ("Helvetica", 24))
+#Pack(puts on top, packs below), Grid(Rows and columns), and Place(specify coordinates)
+
+my_label.pack(pady=20)
+
+my_button= Button(root, text = "Click Me!", font = ("Helvetica", 20))
+my_button.pack(pady=20)
+
+
+root.mainloop()
